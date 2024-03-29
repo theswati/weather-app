ea119683d9be73b2deb3aa6af78112d04a452f3c
Swati <jswati603@gmail.com>
2024-03-29 11:23:39 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+// import images to use it 
+import summer from "./images/summer.jpg"
+import winter from "./images/winter.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+
+     const [latitude,setLatitude]=useState(0) // update the varable
+     const [longitude,setLongitude]=useState(0)
+     const [hemisphere,sethemisphere]=useState("")
+     const [month,setMonth]=useState(()=>{return new Date().getMonth()+1})
+     // hemisphere could only be updated by sethemisphere function
+
+   // Normal variables we dont use it to update in react
+
+
+    function fetchLocation(){
+        if(navigator.geolocation){// checking if this feature exists in browser
+            navigator.geolocation.getCurrentPosition(
+                (position)=>{
+                    //  console.log(position.coords.latitude)
+                    //  console.log(position.coords.longitude)
+
+                    setLatitude(position.coords.latitude)
+                    setLongitude(position.coords.longitude)
+                     
+                    if(position.coords.latitude>0){
+                        // if lat>o it will be Nothen
+                        // adding nort
+                        sethemisphere("Northern Hemisphere")
+                    }
+                    else if (position.coords.latitude<0){
+                        sethemisphere("Southern Hemisphere")
+                    }
+
+                    else{
+                        sethemisphere("Equator")
+                    }
+
+                    
+                }
+            )
+
+        }
+    }
+
+
+return (
+    <div>
+        <button onClick={fetchLocation}>Get Location </button>
+        <h1>latitude:{latitude}</h1>
+        <h1>longitude:{longitude}</h1>
+        <h1>Hemisphere:{hemisphere}</h1>
+        <h1>Month:{month}</h1>
     </div>
-  );
+)
+
+
+
+
 }
 
+
 export default App;
+
+
+
+
+// if latitude>0->Northern Hemisphere
+// if latitude<0->southern Hemisphere
+// if latitude=0->Equator
